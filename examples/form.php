6171a90a17db0e86822bc5809b5cea711a7381c4
VSKut <me@vskut.ru>
2016-05-09 01:21:44 +0300

    Example form method update

--- examples/form.php
+++ examples/form.php
@@ -1,4 +1,4 @@
-<form method="post" action="/examples/oauth.php">
+<form method="get" action="/examples/oauth.php">
     <input type="text" name="phone" value=""><br/>
     <input type="text" name="password" value=""><br/>
     <input type="submit" value="Get sms code">
